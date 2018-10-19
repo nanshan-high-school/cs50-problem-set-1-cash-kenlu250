6b2bd5cee72301878e2b298eb9d3834f51dece81
kenlu250 <43640574+kenlu250@users.noreply.github.com>
2018-10-19 22:57:00 +0800

    零錢問題.cpp

---
+++ .gitignore/零錢問題.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int money = 0;
+  int fifty = 0;
+  int ten = 0;
+  int five = 0;
+  int one = 0;
+
+  cout <<"請輸入金額:";
+  cin >> money;
+
+  while(money >= 50){
+    money = money - 50;
+    fifty++;
+  }
+  cout << "五十元有" << fifty << "個\n";
+  
+  while(money >= 10){
+    money = money - 10;
+    ten++;
+  }
+  cout << "十元有" << ten << "個\n";
+
+  while(money >= 5){
+    money = money - 5;
+    five++;
+  }
+  cout << "五元有" << five << "個\n";
+  
+  while(money >= 1){
+    money = money - 1;
+    one++;
+  }
+  cout << "一元有" << one << "個\n";
+
+  
+    
+}
